fbc44025e2ed432ab73d3360fb04623a112b99b0
Pazeto22 <gustavopazeto95@gmail.com>
2020-12-12 12:51:25 -0300

    Feat: navbar mobile

--- src/components/ui/Navbar/index.jsx
+++ src/components/ui/Navbar/index.jsx
@@ -2,25 +2,35 @@ import React from "react";
 
 import "./styles.scss";
 import logo from "../../../assets/logo.svg";
+import { ReactComponent as Hmenu } from "../../../assets/ico/hmenu.svg";
 import { ReactComponent as SearchIco } from "../../../assets/ico/search.svg";
 import { ReactComponent as UserIco } from "../../../assets/ico/user.svg";
 import { ReactComponent as CartIco } from "../../../assets/ico/cart.svg";
 
 export default function Navbar() {
   return (
-    <section className="navbar-section">
+    <container className="navbar-container">
       <div className="view navbar-wrapper">
-        <img
-          className="logo-corebiz"
-          src={logo}
-          alt="logo da empresa corebiz"
-        />
-        <div className="search-bar">
-          <input type="text" className="search-input" />
-          <button className="search-button">
+        <div className="mobile-menu">
+          <Hmenu />
+        </div>
+        <div className="navbar-logo">
+          <img
+            className="logo-corebiz"
+            src={logo}
+            alt="logo da empresa corebiz"
+          />
+        </div>
+        <form className="search-bar">
+          <input
+            type="search"
+            className="search-input"
+            placeholder="O que está procurando?"
+          />
+          <button type="submit" className="search-button">
             <SearchIco />
           </button>
-        </div>
+        </form>
         <div className="user-account">
           <UserIco />
           <span>Minha Conta</span>
@@ -30,6 +40,6 @@ export default function Navbar() {
           <span>1</span>
         </div>
       </div>
-    </section>
+    </container>
   );
 }
